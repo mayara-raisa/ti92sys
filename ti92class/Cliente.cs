21c254664ad53717984c08b78fb8060702c13788
MAYARA RAISA SOUSA NASCIMENTO <mayararaisada2@icloud.com>
2023-02-16 11:50:21 -0300

    16/02 Endereço

--- ti92class/Cliente.cs
+++ ti92class/Cliente.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Renci.SshNet.Security.Cryptography;
 
 namespace ti92class
 {
@@ -12,15 +13,15 @@ namespace ti92class
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public int Cpf { get; set; }
+        public string Cpf { get; set; }
         public string Email { get; set; }
         public DateTime Data { get; set; }
         public bool Ativo { get; set; }
         public List<Endereco> Enderecos { get; set; }
         public List<Telefone> Telefones { get; set; }
- 
+
         public Cliente() { }
-        public Cliente(int id, string nome, int cpf, string email, DateTime data, bool ativo,List<Endereco>enderecos,List<Telefone> telefones)
+        public Cliente(int id, string nome, string cpf, string email, DateTime data, bool ativo, List<Endereco> enderecos, List<Telefone> telefones)
         {
             Id = id;
             Nome = nome;
@@ -31,7 +32,7 @@ namespace ti92class
             Enderecos = enderecos;
             Telefones = telefones;
         }
-        public Cliente(string nome, int cpf, string email, DateTime data, bool ativo)
+        public Cliente(string nome, string cpf, string email, DateTime data, bool ativo)
         {
             Nome = nome;
             Cpf = cpf;
@@ -40,18 +41,18 @@ namespace ti92class
             Ativo = ativo;
         }
         public Cliente(int id)
-        { 
+        {
             Telefones = Telefone.ListarPorTelefone(id);
             Enderecos = Endereco.ListarPorEndereco(id);
         }
         private void Inserir()
         {
             var cmd = Banco.Abrir();
-            cmd.CommandText = "insert cliente (nome,cpf,email,datacad,ativo) " + 
-            " value ('" + Nome + "'," + Cpf + ",'" + Email + "'," + Data + "," + Ativo +")";
+            cmd.CommandText = "insert cliente (nome,cpf,email,datacad,ativo) " +
+            " value ('" + Nome + "'," + Cpf + ",'" + Email + "'," + Data + "," + Ativo + ")";
             cmd.ExecuteNonQuery();
             cmd.CommandText = "select @@identity";
-            Id = Convert.ToInt32(cmd.ExecuteScalar()); 
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
             foreach (var telefone in Telefones)
             {
                 telefone.Inserir(Id);
@@ -74,13 +75,72 @@ namespace ti92class
                 dr.GetDateTime(4),
                 dr.GetBoolean(5),
                 Endereco.ListarPorEndereco(dr.GetInt32(6)),
-                
+                Telefone.ListarPorTelefone(dr.GetInt32(7))
                )
               );
             }
             return lista;
         }
-
+        public static Cliente ObterPorId(int _id)
+        {
+            Cliente cliente = new Cliente();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * cliente where id = " + _id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cliente.Id = dr.GetInt32(0);
+                cliente.Nome = dr.GetString(1);
+                cliente.Cpf = dr.GetString(2);
+                cliente.Email = dr.GetString(3);
+                cliente.Data = dr.GetDateTime(4);
+                cliente.Ativo = dr.GetBoolean(5);
+                cliente.Enderecos = Endereco.ListarPorEndereco(dr.GetInt32(0));
+                cliente.Telefones = Telefone.ListarPorTelefone(dr.GetInt32(0));
+            }
+            return cliente;
+        }
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update cliente set nome = " +
+            "'" + Nome + "', cpf '" + Cpf + "', email'" + Email + "', data '" + Data + "', ativo'" + Ativo;
+            cmd.ExecuteNonQuery();
+        }
+        public bool Arquivar(int id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update cliente set ativo = 0 whare id = " + id;
+            bool result = cmd.ExecuteNonQuery() == 1 ? true : false;
+            return result;
+        }
+        public static List<Cliente> BuscarPorNome(string _parte)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select *from cliente where nome like '%" + _parte + "%' orde by nome;";
+            var dr = cmd.ExecuteReader();
+            List<Cliente> listar = new List<Cliente>();
+            while (dr.Read())
+            {
+                listar.Add(new Cliente(
+                dr.GetInt32(0),
+                dr.GetString(1),
+                dr.GetString(2),
+                dr.GetString(3),
+                dr.GetDateTime(4),
+                dr.GetBoolean(5),
+                Endereco.ListarPorEndereco(dr.GetInt32(6)),
+                Telefone.ListarPorTelefone(dr.GetInt32(7))
+                    )
+                 );
+               }
+            return listar;
+        }
     }
+
 
 }
--- ti92class/Endereco.cs
+++ ti92class/Endereco.cs
@@ -9,26 +9,50 @@ namespace ti92class
     public class Endereco
     {
         public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
         public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }        
+        public string Uf { get; set; }
         public string Tipo { get; set; }
+        
 
         public Endereco() { }
-        public Endereco(int id, string numero, string tipo)
+        public Endereco(
+        int id,string cep,string logradouro,string numero, string complemento,string bairro, string cidade, string estado , string uf,string tipo)
         {
             Id = id;
+            Cep= cep;
+            Logradouro = logradouro;
             Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Uf = uf;
             Tipo = tipo;
+
         }
-        public Endereco(string numero, string tipo)
+        public Endereco(string cep, string logradouro, string numero, string complemento, string bairro, string cidade, string estado, string uf, string tipo)
         {
+            Cep = cep;
+            Logradouro = logradouro;
             Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Uf = uf;
             Tipo = tipo;
         }
         public void Inserir(int cliente_id)
         {
             var cmd = Banco.Abrir();
             cmd.CommandText = "inserir tefones(cliente_id,numeros,tipos)" +
-                " value (" + cliente_id + "'" + Numero + "','" + Tipo + "')";
+           " value (" + cliente_id + "'" + Cep + "','" + Logradouro + "', '" +Numero+ "', '"+Complemento+"')";
             cmd.ExecuteNonQuery();
             cmd.Connection.Close();
         }
@@ -48,6 +72,17 @@ namespace ti92class
                     );
             }
             return listaTel;
+        }
+        public static Endereco ObterPorId(int _id)
+        {
+           Endereco endereco = new Endereco();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from endercos where id = " + _id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+             
+            }
         }
 
     }
